ef961fd56f66373da447bf9b964bcb9e4a1ef4b4
yyeung13 <33860110+yyeung13@users.noreply.github.com>
2020-02-24 17:48:37 +0800

    Update GreetingController.java

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -9,11 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-	private static final String template = "Good Afternoon, %s!";
+	private static final String template = "Good Evening, %s!";
 	private final AtomicLong counter = new AtomicLong();
+
+	private static final org.slf4j.Logger LOGGER = org.slf4j.LoggerFactory.getLogger(GreetingController.class);	
 
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+		int cnt = (int) counter.incrementAndGet();
+		
+		LOGGER.info("--[/greeting] id : " + cnt + ", name: " + name + " ---");
+		return new Greeting(cnt, String.format(template, name));
 	}
 }
